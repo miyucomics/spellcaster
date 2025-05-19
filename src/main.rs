d6de6da1f515626841543e61bfb86507fe2935d3
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-05-19 16:17:13 -0400

    Improved score calculation, no reiteration

--- src/main.rs
+++ src/main.rs
@@ -5,148 +5,153 @@ mod trie;
 use std::{cmp::Reverse, collections::HashSet};
 use trie::Trie;
 
-static LETTER_VALUES: [u32; 26] = [
+static LETTER_VALUES: [u8; 26] = [
     1, 4, 5, 3, 1, 5, 3, 4, 1, 7, 3, 3, 4, 2, 1, 4, 8, 2, 2, 2, 4, 5, 5, 7, 4, 8,
 ];
 
 struct Solution {
     word: Vec<u8>,
-    breadcrumbs: Vec<u8>,
+    path: Vec<u8>,
+    score: u8,
 }
 
-struct TraverseContext<'a> {
+impl Solution {
+    fn get_word(&self) -> String {
+        self.word
+            .iter()
+            .map(|&byte| (byte + b'a') as char)
+            .collect::<String>()
+    }
+}
+
+struct Context<'a> {
     trie: &'a Trie,
-    board: &'a Vec<u8>,
+    board: &'a [u8; 25],
     neighbors: &'a Vec<Vec<u8>>,
 
-    bitmask: u32,
+    board_mask: u32,
+
     solutions: &'a mut Vec<Solution>,
-    word_accumulator: &'a mut Vec<u8>,
-    breadcrumb_accumulator: &'a mut Vec<u8>,
-}
+    word_builder: &'a mut Vec<u8>,
+    trail_builder: &'a mut Vec<u8>,
 
-fn score_solution(
-    solution: &Solution,
-    double_letter_tile: Option<u8>,
-    triple_letter_tile: Option<u8>,
-    double_word_tile: Option<u8>,
-) -> u32 {
-    let mut score: u32 = 0;
-
-    for (&ch, &pos) in solution.word.iter().zip(&solution.breadcrumbs) {
-        let mut multiplier = 1;
-
-        if Some(pos) == double_letter_tile {
-            multiplier = 2;
-        } else if Some(pos) == triple_letter_tile {
-            multiplier = 3;
-        }
-
-        score += LETTER_VALUES[ch as usize] * multiplier;
-    }
-
-    if let Some(dw) = double_word_tile
-        && solution.breadcrumbs.contains(&dw)
-    {
-        score *= 2;
-    }
-
-    if solution.word.len() >= 6 {
-        score += 10;
-    }
-
-    score
+    double_letter: Option<u8>,
+    triple_letter: Option<u8>,
+    double_word: Option<u8>,
 }
 
 fn traverse(
     trie_index: usize,
     board_position: u8,
-    swaps_remaining: u8,
-    context: &mut TraverseContext,
+    swaps: u8,
+    score: u8,
+    double: bool,
+    context: &mut Context,
 ) {
     let current_letter = context.board[board_position as usize];
     let current_node = &context.trie.nodes[trie_index];
 
     for (letter_needed, &child_option) in current_node.children.iter().enumerate() {
         let letter_needed = u8::try_from(letter_needed).unwrap();
-        if let Some(child_trie_index) = child_option {
+        if let Some(child_index) = child_option {
             let cost = u8::from(current_letter != letter_needed);
-            if swaps_remaining < cost {
+            if swaps < cost {
                 continue;
             }
 
-            context.word_accumulator.push(letter_needed);
-            context.breadcrumb_accumulator.push(board_position);
+            let multiplier = if Some(board_position) == context.double_letter {
+                2
+            } else if Some(board_position) == context.triple_letter {
+                3
+            } else {
+                1
+            };
 
-            if context.trie.nodes[child_trie_index as usize].is_terminal {
+            let next_score = score + LETTER_VALUES[letter_needed as usize] * multiplier;
+            let has_encountered_double = double || Some(board_position) == context.double_word;
+
+            context.word_builder.push(letter_needed);
+            context.trail_builder.push(board_position);
+
+            if context.trie.nodes[child_index as usize].is_terminal {
+                let mut final_score = next_score;
+                if has_encountered_double {
+                    final_score *= 2;
+                }
+                if context.word_builder.len() >= 6 {
+                    final_score += 10;
+                }
+
                 context.solutions.push(Solution {
-                    word: context.word_accumulator.clone(),
-                    breadcrumbs: context.breadcrumb_accumulator.clone(),
+                    word: context.word_builder.clone(),
+                    path: context.trail_builder.clone(),
+                    score: final_score,
                 });
             }
 
-            context.bitmask |= 1 << board_position;
+            context.board_mask |= 1 << board_position;
             for neighbor in context.neighbors.get(board_position as usize).unwrap() {
-                if context.bitmask & (1 << neighbor) == 0 {
+                if context.board_mask & (1 << neighbor) == 0 {
                     traverse(
-                        child_trie_index as usize,
+                        child_index as usize,
                         *neighbor,
-                        swaps_remaining - cost,
+                        swaps - cost,
+                        next_score,
+                        has_encountered_double,
                         context,
                     );
                 }
             }
-            context.bitmask &= !(1 << board_position);
+            context.board_mask &= !(1 << board_position);
 
-            context.word_accumulator.pop();
-            context.breadcrumb_accumulator.pop();
+            context.word_builder.pop();
+            context.trail_builder.pop();
         }
     }
 }
 
 fn solve_board(
-    board: &Vec<u8>,
-    dl: Option<u8>,
-    tl: Option<u8>,
-    dw: Option<u8>,
-    swaps: u8,
+    board: &[u8; 25],
     trie: &Trie,
     neighbors: &Vec<Vec<u8>>,
+
+    swaps: u8,
+
+    double_letter: Option<u8>,
+    triple_letter: Option<u8>,
+    double_word: Option<u8>,
 ) {
     let mut solutions = Vec::new();
-    let mut word_accumulator = Vec::with_capacity(25);
-    let mut breadcrumb_accumulator = Vec::with_capacity(25);
+    let mut word_builder = Vec::with_capacity(25);
+    let mut trail_builder = Vec::with_capacity(25);
 
-    let mut context = TraverseContext {
+    let mut context = Context {
         trie,
         board,
         neighbors,
-        bitmask: 0,
+        board_mask: 0,
+
         solutions: &mut solutions,
-        word_accumulator: &mut word_accumulator,
-        breadcrumb_accumulator: &mut breadcrumb_accumulator,
+        word_builder: &mut word_builder,
+        trail_builder: &mut trail_builder,
+
+        double_letter,
+        triple_letter,
+        double_word,
     };
 
     for pos in 0..25 {
-        traverse(0, pos, swaps, &mut context);
+        traverse(0, pos, swaps, 0, false, &mut context);
     }
 
     let mut seen = HashSet::new();
     solutions.retain(|s| seen.insert(s.word.clone()));
-    solutions.sort_by_key(|s| Reverse(score_solution(s, dl, tl, dw)));
+    solutions.sort_by_key(|s| Reverse(s.score));
 
-    for solution in solutions.iter().take(10) {
-        let score = score_solution(solution, dl, tl, dw);
-        println!(
-            "{:<13} {score:<5} {:?}",
-            solution
-                .word
-                .iter()
-                .map(|&byte| (byte + b'a') as char)
-                .collect::<String>(),
-            solution.breadcrumbs,
-        );
+    for word in solutions.iter().take(10) {
+        println!("{:<13} {:<5} {:?}", &word.get_word(), word.score, word.path);
     }
+
     println!();
 }
 
@@ -157,7 +162,7 @@ fn main() {
         .split_ascii_whitespace()
         .for_each(|word| trie.insert(word));
 
-    let neighbor_cache: Vec<Vec<u8>> = (0..25)
+    let neighbors: Vec<Vec<u8>> = (0..25)
         .map(|pos| {
             let x = pos % 5;
             let y = pos / 5;
@@ -179,12 +184,8 @@ fn main() {
         })
         .collect();
 
-    let board = "tssoknodovampenstxegzwtyi"
-        .bytes()
-        .map(|byte| byte - b'a')
-        .collect();
-
-    solve_board(&board, Some(14), None, None, 0, &trie, &neighbor_cache);
-    solve_board(&board, Some(14), None, None, 1, &trie, &neighbor_cache);
-    solve_board(&board, Some(14), None, None, 2, &trie, &neighbor_cache);
+    let board = b"tssoknodovampenstxegzwtyi".map(|byte| byte - b'a');
+    solve_board(&board, &trie, &neighbors, 0, Some(14), None, None);
+    solve_board(&board, &trie, &neighbors, 1, Some(14), None, None);
+    solve_board(&board, &trie, &neighbors, 2, Some(14), None, None);
 }
